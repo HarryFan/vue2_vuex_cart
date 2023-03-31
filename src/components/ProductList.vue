d14b55bd1d2a5aa343cf6eda4aa15ad96ac70fd4
Harry <harry750110@gmail.com>
2023-03-31 18:15:45 +0800

    新增註解

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -14,31 +14,38 @@
   </div>
 </template>
 <script>
-export default {
-  props: ['product'],
-  methods: {
-    addToCart() {
-      // 呼叫 ADD_TO_CART mutation
-      this.$store.commit('ADD_TO_CART', { id: this.product.id, quantity: 1 });
+  // 這是一個 Vue 組件，名為 ProductList，主要用於顯示產品清單，包括產品圖片、產品名稱、產品價格以及加入購物車按鈕。這個組件接收一個名為 product 的 prop，用於顯示一個產品的相關資訊。
+  // 在這個組件中，我們使用了 Vue 的 props 和 methods 屬性。props 是用來接收父組件傳遞過來的資料，這裡的 product prop 是一個物件，包含了產品的相關資訊。methods 是用來定義一些方法，這裡定義了一個名為 addToCart 的方法，用於將產品加入購物車。
+  // 在 addToCart 方法中，我們呼叫了 this.$store.commit 方法，並傳遞了一個名為 ADD_TO_CART 的 mutation 和一個包含產品 ID 和數量的物件。mutation 是用來修改 Vuex store 的狀態，這裡的 ADD_TO_CART mutation 是將一個產品加入購物車。
+  // 在這個組件的模板中，我們使用了 Vue 的指令來顯示產品的相關資訊。v-bind 指令用於綁定產品圖片的 src 屬性，使其顯示產品圖片。{{ }} 雙括號語法用於顯示產品名稱和價格。v-on 指令用於監聽加入購物車按鈕的點擊事件，當按鈕被點擊時，會呼叫 addToCart 方法。
+  export default {
+    props: ['product'],
+    methods: {
+      addToCart() {
+        // 呼叫 ADD_TO_CART mutation
+        this.$store.commit('ADD_TO_CART', {
+          id: this.product.id,
+          quantity: 1
+        });
+      }
     }
   }
-}
 </script>
 
 <style scope>
-.product-list {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-}
+  .product-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
 
-.product {
-  border: 1px solid #ccc;
-  padding: 1rem;
-  flex: 1 1 300px;
-}
+  .product {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    flex: 1 1 300px;
+  }
 
-.product img {
-  max-width: 100%;
-}
+  .product img {
+    max-width: 100%;
+  }
 </style>
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -10,6 +10,15 @@
   </div>
 </template>
 <script>
+// 這段程式碼定義了一個顯示購物車內容的 Vue 組件。以下是各個部分的說明：
+// <h2>購物車</h2>：標題，顯示在購物車區塊的頂部。
+// <ul>：用來顯示購物車內容的無序列表。
+// v-for="product in cartProducts" :key="product.id"：使用 v-for 指令迭代購物車中的產品，並為每個產品指定一個獨特的鍵。
+// {{ product.title }} - {{ product.price }} x {{ product.quantity }} = {{ product.price * product.quantity }}：顯示產品的名稱、價格、數量和小計。
+// <p>總計：{{ cartTotalPrice }}</p>：顯示購物車的總計金額。
+// cartProducts()：從 Vuex store 的 cart state 中取得產品數組。
+// cartTotalPrice()：從 Vuex store 的 cart state 中計算產品的總金額，並使用 toFixed(2) 方法將結果格式化為小數點後兩位的字串。
+
 export default {
   computed: {
     cartProducts() {
